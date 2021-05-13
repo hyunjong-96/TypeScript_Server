b1264012363233768ffac63e362ee07d23846298
hyunjong_96 <ahajongs@gmail.com>
2021-05-13 20:43:01 +0900

    #api/ 학력추가

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,7 +1,9 @@
-import express from 'express'
+import express,{Request,Response} from 'express'
 import config from '../config'
 import request from 'request'
 import Profile from '../models/Profile'
+import auth from '../middleware/auth'
+import { check, validationResult } from 'express-validator'
 
 const router = express.Router();
 
@@ -51,6 +53,58 @@ router.get("/user/:user_id",async(req,res)=>{
         res.status(500).send("Server Error")
     }
 })
+
+/**
+ *  @route Put api/profile/experience
+ *  @desc Add profile experience
+ *  @access Private
+ */
+
+router.put(
+    "/experience",
+    auth,
+    [
+        check("school","School is required").not().isEmpty(),
+        check("degree","Degree is required").not().isEmpty(),
+        check("fieldofstudy","Field of study is required").not().isEmpty(0)
+    ],
+    async(req:Request, res:Response)=>{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+        const{
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description,
+            user
+        } = req.body;
+
+        const newEdu={
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        }
+
+        try{
+            const profile = await Profile.findOne({user:user.id})
+            profile.education.unshift(newEdu)   //profile객체의 education 배열필드를 뒤쪽이 아닌 앞쪽으로 추가.
+            await profile.save()
+            res.json(profile)
+        }catch(err){
+            console.error(err.message)
+            res.status(500).send("Server Error")
+        }
+    }
+)
 
 module.exports = router;
 
